5958473cc85705823b5fccb560b124351253c411
guerillaxgardener <maker.mdm@gmail.com>
2024-08-30 15:05:55 -0600

    update to have last clock in time

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,7 @@ const App: React.FC = () => {
         setAdditionalMinutes((prev) => ({ ...prev, [day]: minutesWorked }));
       }
     },
-    [lastClockIn]
+    []
   );
 
   useEffect(() => {
